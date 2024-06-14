d2adb6cec58ed3e2ced0939073edc6e5ed657573
Mike Wehinger <mikewehinger@yahoo.com>
2024-06-14 20:37:27 +1000

    added a daily % change var and bar chart

--- PyFi.py
+++ PyFi.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 # Determine the ticker and period of time
 ticker = 'BCOV'
 end_date = pd.Timestamp.today()
-start_date = end_date - pd.DateOffset(months=2)
+start_date = end_date - pd.DateOffset(months=6)
 
 # Get stock data from yahoo finance
 print("Getting data...")
@@ -15,6 +15,11 @@ data = yf.download(ticker, start=start_date, end=end_date)
 
 # Calculate the 10-day moving average
 data['10_day_MA'] = data['Close'].rolling(window=10).mean()
+
+# Calculate the daily percentage change
+data['Daily % Change'] = data['Close'].pct_change() * 100
+# Remove the first row with NaN values resulting from pct_change()
+data = data.dropna()
 
 # Create a Plotly candlestick chart
 fig = go.Figure(data=[go.Candlestick(x=data.index,
@@ -38,6 +43,20 @@ fig.update_layout(
 )
 
 # Display chart
-print("Displaying chart")
+print("Displaying candlestick chart")
 fig.show()
+
+
+fig_bar = go.Figure(data=[go.Bar(x=data.index, y=data['Daily % Change'], name='Daily % Change')])
+
+# Add titles and labels to the bar chart
+fig_bar.update_layout(
+    title='Daily Percentage Change BCOV',
+    xaxis_title='Date',
+    yaxis_title='Daily % Change'
+)
+
+print("Displaying candlestick chart")
+# Display the bar chart
+fig_bar.show()
 
